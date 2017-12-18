b9e7aae0f9777c7a4fa1b019ace3064a6c7d73f6
root <root@localhost.localdomain>
2017-12-18 13:14:25 -0600

    All files Committed with code cleanup

--- versalex/src/main/ansible/library/schedule_actions.py
+++ versalex/src/main/ansible/library/schedule_actions.py
@@ -33,12 +33,14 @@ def get_jsonoutput(json_path,render_content):
 	except Exception,e:
 		print "Exception on get Json Output",e
 
-def schedule_actions(server_hosts,schedule_option,action_type):
+def schedule_actions(server_hosts,schedule_option,action_type,total_actions):
 	try:
 		action_json_req= {}
 		for srv_host in server_hosts:
 			action_base_url="http://"+srv_host+":5080/api/actions"
-			get_url ='http://'+srv_host+':5080/api/actions?filter=alias eq "'+action_type+'"'
+			print "Host to Post URL",srv_host
+			#http://165.227.81.57:5080/api/actions?filter=alias eq "send"&count=10000
+			get_url ='http://'+srv_host+':5080/api/actions?filter=alias eq "'+action_type+'"'+'&count='+total_actions
 			#get_url ='http://'+srv_host+':5080/api/actions'
 			print "URL to enable Send Actions",get_url
 			head ={ 'Content-type':'application/json','Accept':'application/json'}
@@ -53,7 +55,7 @@ def schedule_actions(server_hosts,schedule_option,action_type):
 					action_json_req['schedule_option']=schedule_option
 					action_json_req['alias']=action_type
 					put_action=action_base_url+"/"+each_id['id']
-					action_output=get_jsonoutput("./files/Sched_Action.json",action_json_req)
+					action_output=get_jsonoutput("../files/Sched_Action.json",action_json_req)
 					sched_action_results = requests.put(put_action,headers=head,auth=HTTPBasicAuth('administrator', 'Admin'),data=action_output)
 					print "Schedule Actions Response",sched_action_results					
 			else:
@@ -65,15 +67,17 @@ def schedule_actions(server_hosts,schedule_option,action_type):
 
 def main():
  	try:
+		# stat,result=schedule_actions(["165.227.81.57"],"on file polling continuously","send")
  		fields = {
  			"hosts": {"required": True, "type": "list"},
 			"schedule_option": {"required": True, "type": "str"},
 			"action_type": {"required": True, "type": "str"},
+			"total_actions": {"required": True, "type": "str"},
  		}
  		module = AnsibleModule(argument_spec=fields)
-		stat,result=schedule_actions(module.params['hosts'],module.params['schedule_option'],module.params['action_type'])
+		stat,result=schedule_actions(module.params['hosts'],module.params['schedule_option'],module.params['action_type'],module.params['total_actions'])
  		if stat:
- 			module.exit_json(meta=result)
+			module.exit_json(meta=result)
  		else:
  			module.fail_json(meta=result)
  	except Exception,e:
